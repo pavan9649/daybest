5ffadd87fd419fe83ab37cefc597fd7051c7fd00
Pavan Saini <ps9115981@gmail.com>
2022-02-15 12:58:04 +0530

    sixcommit

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,9 +1,16 @@
 const userController = require('../controller/userController')
 const express = require('express');
 const router = express.Router();
+const {
+    verifyToken,
+    verifyTokenAndAuthorization,
+    verifyTokenAndAdmin,
+  } = require("../middleware/middle");
+
 
 router.post("/signup",userController.signup);
 router.post("/signin",userController.signin);
+router.get("/logout",verifyToken,userController.logout);
 router.route("/password/forgot").post(userController.forgotPassword);
 
 router.route("/password/reset/:token").put(userController.resetPassword);
