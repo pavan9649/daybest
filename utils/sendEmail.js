4dfc2f68a7d14084fbf6aa1dc79de744101dce28
Pavan Saini <ps9115981@gmail.com>
2022-02-24 15:06:33 +0530

    change

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -2,17 +2,17 @@ const nodeMailer = require("nodemailer");
 
 const sendEmail = async (options) => {
   const transporter = nodeMailer.createTransport({
-    host: process.env.SMPT_HOST,
-    port: process.env.SMPT_PORT,
-    service: process.env.SMPT_SERVICE,
+    host: "smtp.gmail.com",
+    port: 587,
+    service: "gmail",
     auth: {
-      user: process.env.SMPT_MAIL,
-      pass: process.env.SMPT_PASSWORD,
+      user: "ps9115981@gmail.com",
+      pass: "Suman@1212",
     },
   });
 
   const mailOptions = {
-    from: process.env.SMPT_MAIL,
+    from: "ps9115981@gmail.com",
     to: options.email,
     subject: options.subject,
     text: options.message,
