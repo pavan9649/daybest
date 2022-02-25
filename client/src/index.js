73d37111411abf84fa9e37b719f1fa3c3922d48e
Pavan Saini <ps9115981@gmail.com>
2022-02-25 11:37:57 +0530

    chack

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,14 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
+
+import {
+
+  HashRouter
+ } from "react-router-dom";
+  
+ ReactDOM.render(<HashRouter><App /></HashRouter>, document.getElementById("root"));
+ 
 ReactDOM.render(
     <BrowserRouter>
       <App />
