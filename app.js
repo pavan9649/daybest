ff6761fa1542bd1f71a3b537b0ece0454d1c483c
Pavan Saini <ps9115981@gmail.com>
2022-02-25 12:09:00 +0530

    fix refresh problem

--- app.js
+++ app.js
@@ -89,6 +89,9 @@ process.on("uncaughtException", (err) => {
    if(process.env.NODE_ENV="production")
    {
      app.use(express.static("client/build"))
+     app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
 
    }
 
